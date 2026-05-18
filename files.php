73da29fd26d7bb1db32598c8fb6a512dabacc4f1
nobita <jescriba.0126@umak.edu.ph>
2026-05-19 02:57:52 +0800

    new

---
+++ files.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>ACTIVITY</title>
+</head>
+
+<body>
+
+<form action="activity3.php" method="post">
+
+<b>Enter a Number:</b>
+<input type="text" name="number" size="20"/>
+
+<p>
+<input type="submit" name="submit" value="Factorial">
+</p>
+
+</form>
+
+</body>
+</html>
+
+
